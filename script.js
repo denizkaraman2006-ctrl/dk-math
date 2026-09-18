ca258f4d0c5a4e0b1973d17ced65eb2e4568d1ba
Deniz Karaman <deniz.karaman2006l@gmail.com>
2026-09-18 13:55:40 +0200

    Poprawiono statystyki DK Math

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 fetch("/stats")
+
 .then(res => res.json())
+
 .then(data => {
 
-document.getElementById("lessons").innerText =
-data.bookings || 0;
+    document.getElementById("lessons").innerText =
+        "15+";
 
-document.getElementById("students").innerText =
-data.users || 0;
+    document.getElementById("students").innerText =
+        "10+";
 
-document.getElementById("rating").innerText =
-95;
+    document.getElementById("rating").innerText =
+        "100%";
 
 });
